d6eff3decb1c898dfd9d7fb1d63b6a02cc49c5e4
Levente <leventea@tuta.io>
2023-03-27 19:52:51 +0200

    Configure GSON to use the new LocalDate adapter

--- src/main/java/com/example/cinemaapp/rest/RetrofitSingleton.java
+++ src/main/java/com/example/cinemaapp/rest/RetrofitSingleton.java
@@ -2,18 +2,28 @@ package com.example.cinemaapp.rest;
 
 // import Retrofit
 
+import com.example.cinemaapp.adapter.LocalDateAdapter;
+import com.google.gson.GsonBuilder;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.time.LocalDate;
 
 public class RetrofitSingleton {
     private static Retrofit retrofitInstance = null;
 
     public static Retrofit getInstance() {
-        if (retrofitInstance == null)
+        if (retrofitInstance == null) {
+            var gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
+
+            var gsonInstance = gsonBuilder.create();
+
             retrofitInstance = new Retrofit.Builder()
                     .baseUrl("https://vr-api.leventea.hu/")
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gsonInstance))
                     .build();
+        }
 
         return retrofitInstance;
     }
