48077556b172bebffd13e037a1a0691cf2ca5014
KlementSzabolcs1 <klementszabolcs1@gmail.com>
2023-04-14 09:57:03 +0200

    refactoring

--- src/main/java/com/example/cinemaapp/rest/auth/TokenManager.java
+++ src/main/java/com/example/cinemaapp/rest/auth/TokenManager.java
@@ -1,17 +1,9 @@
 package com.example.cinemaapp.rest.auth;
-import com.example.cinemaapp.Auditorium.Auditorium;
 import com.example.cinemaapp.rest.RetrofitSingleton;
 import com.google.gson.Gson;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 import java.io.IOException;
 import java.util.Base64;
-import java.util.List;
 import java.util.Objects;
-
-
 public class TokenManager {
     private static String accessToken;
     private static String refreshToken;
@@ -31,45 +23,33 @@ public class TokenManager {
         refreshToken = tokens.refreshToken;
 
         user = parseJwt(accessToken);
-        System.out.println(user.role);
         if (!Objects.equals(user.role, "admin")){
             return 2;//2 if user's role is not admin
         }
         return 3;//3 if response is successful and the user is an admin
     }
-
     public static User parseJwt(String accessToken) {
         var chunks = accessToken.split("\\.");
         var decoder = Base64.getUrlDecoder();
         var body = new String(decoder.decode(chunks[1]));
-
         var gson = new Gson();
-        User user = gson.fromJson(body, User.class);
-
-        return user;
+        return gson.fromJson(body, User.class);
     }
-
     public static long getUnixTime() {
         return System.currentTimeMillis() / 1000L;
     }
-
     public static String refreshToken() throws IOException {
         var authService = RetrofitSingleton.getInstance().create(AuthService.class);
         var resp = authService.refresh(new RefreshToken(refreshToken)).execute();
-
         if (!resp.isSuccessful())
             throw new RuntimeException("failed to refresh access token");
-
         return resp.body();
     }
-
     public static String getAccessToken() throws IOException {
         var adjustedExp = user.exp - 30;
-
         if (getUnixTime() > adjustedExp) {
             accessToken = refreshToken();
         }
-
         return "Bearer " + accessToken;
     }
 }
