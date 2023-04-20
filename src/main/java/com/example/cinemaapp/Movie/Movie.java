bac2aeb497d899d5a0c44a9e4942f9fb258d6835
KlementSzabolcs1 <klementszabolcs1@gmail.com>
2023-04-20 13:22:27 +0200

    Made another Movie() since Urls are optional

--- src/main/java/com/example/cinemaapp/Movie/Movie.java
+++ src/main/java/com/example/cinemaapp/Movie/Movie.java
@@ -3,7 +3,7 @@ package com.example.cinemaapp.Movie;
 import java.util.Optional;
 
 public class Movie {
-    public String id, title, subtitle, description;
+    public String id, title, subtitle, description, thumbnailUrl, bannerUrl;
     public int durationMins;
 
     public Movie(String title, String subtitle, String description, int durationMins) {
@@ -11,6 +11,14 @@ public class Movie {
         this.subtitle = subtitle;
         this.description = description;
         this.durationMins = durationMins;
+    }
+    public Movie(String title, String subtitle, String description, int durationMins, String thumbnailUrl, String bannerUrl) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+        this.durationMins = durationMins;
+        this.thumbnailUrl = thumbnailUrl;
+        this.bannerUrl = bannerUrl;
     }
 
     @Override
