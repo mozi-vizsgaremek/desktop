3831d65be1ef6c7a5ee4f1c5a2bca2009954d446
KlementSzabolcs1 <klementszabolcs1@gmail.com>
2023-04-20 13:22:56 +0200

    Calls' response was Person, changed it to Movies

--- src/main/java/com/example/cinemaapp/Movie/MoviesCRUD.java
+++ src/main/java/com/example/cinemaapp/Movie/MoviesCRUD.java
@@ -1,7 +1,5 @@
 package com.example.cinemaapp.Movie;
 
-import com.example.cinemaapp.Auditorium.Auditorium;
-import com.example.cinemaapp.Person.Person;
 import retrofit2.Call;
 import retrofit2.http.*;
 
@@ -9,20 +7,20 @@ import java.util.List;
 
 public interface MoviesCRUD {
     @GET("/movie/")
-    Call<List<Person>> getMovies(@Header("Authorization") String accessKey);
+    Call<List<Movie>> getMovies(@Header("Authorization") String accessKey);
 
     @POST("/movie/")
-    Call<List<Person>> createMovie(@Header("Authorization") String accessKey, @Body Movie movie);
+    Call<Movie> createMovie(@Header("Authorization") String accessKey, @Body Movie movie);
 
     @DELETE("/movie/{id}")
     Call<Void> deleteMovie(@Header("Authorization") String accessKey, @Path("id") String id);
 
     @POST("/movie/{id}/images")
-    Call<List<Person>> createImage(@Header("Authorization") String accessKey, @Body Movie movie, @Path("id") String id);
-    //might need to change @Body Movie to Image
+    Call<MovieUrlDto> addImage(@Header("Authorization") String accessKey, @Body MovieImageDto movieImageDto,
+                        @Path("id") String id);
 
     @DELETE("/movie/{id}/images/{imageSelector}")
-    Call<Void> deleteMovieImage(@Header("Authorization") String accessKey, @Path("id") String id,
+    Call<Movie> deleteMovieImage(@Header("Authorization") String accessKey, @Path("id") String id,
                                 @Path("imageSelector") String imageSelector);
 }
 
