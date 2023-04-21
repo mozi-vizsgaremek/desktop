0720109284c73788b2736b904b3898454b22e7d6
KlementSzabolcs1 <klementszabolcs1@gmail.com>
2023-04-21 15:29:58 +0200

    Implementing the installer package does not work, will need to be fixed ASAP

---
+++ src/main/java/com/example/cinemaapp/Main.java
@@ -0,0 +1,9 @@
+package com.example.cinemaapp;
+
+import com.example.cinemaapp.Login.LoginApp;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginApp.main(args);
+    }
+}
